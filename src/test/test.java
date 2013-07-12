34de0e991637fedfbf78d9a64e512f3ee0074e6d
joro555 <go60_glg@dir.bg>
2013-07-13 00:57:32 +0300

    Test ot Joro
    Ve4e ima Hello World

--- src/test/test.java
+++ src/test/test.java
@@ -25,6 +25,7 @@ public class test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.getWriter().println("Hello World");
 	}
 
 	/**
