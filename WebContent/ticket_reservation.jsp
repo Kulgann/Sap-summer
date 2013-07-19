942a272fc4429fcd6e7d85f09d4b5b5e490d4d51
lainovlopata <pong_x@abv.bg>
2013-07-19 14:03:13 +0300

    update

--- WebContent/ticket_reservation.jsp
+++ WebContent/ticket_reservation.jsp
@@ -7,21 +7,45 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<form action="TicketReservationServlet" name=form1 method="post">
-		<table>
+	<script>
+function handleClick(evt) {
+ document.location.href="sap.project1.TicketReservationServlet?buttonNum=node.value"
+}
+
+</script>
+
+	<table id="table1" border="1" onclick="handleClick(event);">
+		<thead>
+			<tr>
+				<th>Select
+		</thead>
+		<tbody>
 			<tr>
 				<td>
-					<input type="button" name=button1></input>
-				</td>
-				<td>
-					<input type="button" name=button2></input>
-				</td>
-			</tr>
+					<form name="f1" action="#">
+						<input id="edit1" type="submit" name="1" value="1"> <input
+							id="edit1" type="submit" name="2" value="2"> <input
+							id="edit1" type="submit" name="3" value="3"> <input
+							id="edit1" type="submit" name="4" value="4">
+
+					</form>
 			<tr>
-				<td><input type="button" name=button3></input></td>
-				<td><input type="button" name=button4></input></td>
-			</tr>
-		</table>
-	</form>
+				<td>
+					<form name="f2" action="#">
+						<input id="edit2" type="submit" name="5" value="5"> <input
+							id="edit1" type="submit" name="6" value="6"> <input
+							id="edit1" type="submit" name="7" value="7"> <input
+							id="edit1" type="submit" name="8" value="8">
+					</form>
+			<tr>
+				<td>
+					<form name="f3" action="#">
+						<input id="edit3" type="submit" name="9" value="9"> <input
+							id="edit1" type="submit" name="10" value="10"> <input
+							id="edit1" type="submit" name="11" value="11"> <input
+							id="edit1" type="submit" name="12" value="12">
+					</form>
+		</tbody>
+	</table>
 </body>
 </html>
--- src/sap/project1/TicketReservationServlet.java
+++ src/sap/project1/TicketReservationServlet.java
@@ -1,23 +1,32 @@
 package sap.project1;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import sap.project1.TicketControl.ReservedTicket;
 
 /**
  * Servlet implementation class TicketReservationServlet
  */
 public class TicketReservationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	public static List<TicketControl.ReservedTicket> reservedTickets;
+    String selectedButton;
+    int tempRow;
+    int tempCol;
     /**
      * @see HttpServlet#HttpServlet()
      */
     public TicketReservationServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
 	/**
@@ -31,7 +40,73 @@ public class TicketReservationServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		selectedButton = request.getParameter("buttonNum");
+		if(selectedButton.equals("1"))
+		{
+			tempRow = 3;
+			tempCol = 1;
+		}
+		else if(selectedButton.equals("2"))
+		{
+			tempRow = 3;
+			tempCol = 2;
+		}
+		else if(selectedButton.equals("3"))
+		{
+			tempRow = 3;
+			tempCol = 3;
+		}
+		else if(selectedButton.equals("4"))
+		{
+			tempRow = 3;
+			tempCol = 4;
+		}
+		else if(selectedButton.equals("5"))
+		{
+			tempRow = 2;
+			tempCol = 1;
+		}
+		else if(selectedButton.equals("6"))
+		{
+			tempRow = 2;
+			tempCol = 2;
+		}
+		else if(selectedButton.equals("7"))
+		{
+			tempRow = 2;
+			tempCol = 3;
+		}
+		else if(selectedButton.equals("8"))
+		{
+			tempRow = 2;
+			tempCol = 4;
+		}
+		else if(selectedButton.equals("9"))
+		{
+			tempRow = 1;
+			tempCol = 1;
+		}
+		else if(selectedButton.equals("10"))
+		{
+			tempRow = 1;
+			tempCol = 2;
+		}
+		else if(selectedButton.equals("11"))
+		{
+			tempRow = 1;
+			tempCol = 3;
+		}
+		else if(selectedButton.equals("12"))
+		{
+			tempRow = 1;
+			tempCol = 4;
+		}
+		HttpSession session = request.getSession(true);
+		reservedTickets.add(new ReservedTicket((String)session.getAttribute("userId"),
+																(String)session.getAttribute("projectionId"),
+																	tempRow,
+																		tempCol,
+																			new Date()));
 	}
 
 }
